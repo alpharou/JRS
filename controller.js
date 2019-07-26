e51c77fc5be32d9631c327fc9fdf8d568869676b
alpharou <alpharou.dev@gmail.com>
2019-07-26 03:03:44 +0200

    Added placeholder for the exponential moving average model

--- controller.js
+++ controller.js
@@ -2,13 +2,17 @@ class Controller {
 	
 	constructor() {
 		
-		this.machines = [new Machine];
+		this.machines = [new Machine(this)];
+		this.machineCount = 1;
+		this.busyMachines = [];
+		this.freeMachines = this.machines;
 		this.data = [];
 		this.dataStatus = [];
 		this.controllerStatus = "OFF";
 		this.avg = 0;
+		this.queryQueue = [];
 		
-		/*The controller can be in:
+		/*The controller can be in states:
 		DONE: Sorting done
 		WORK: Waiting for step
 		OFF: Waiting for all parameters and machines to be set
@@ -22,8 +26,8 @@ class Controller {
 		
 		for (let i = 0; i < n; i++) {
 			
-			this.machines[i] = new Machine;
-			
+			this.machines[i] = new Machine(this);
+			this.machines[i].setZone();			
 		}
 		
 		this.controllerStatus = "OFF";
@@ -31,17 +35,98 @@ class Controller {
 		
 	}
 	
-	check() {
+	set(n) {
 		
-		if (this.machines.length == 0) {return false;}
+		this.setMachines();
+		if (this.busyMachines.length + this.freeMachines.length != this.machines.length) {return false;}
 		if (this.data.length == 0) {return false;}
 		if (this.dataStatus.length != this.data.length) {return false;}
+		
+		this.controllerStatus = "WORK";
+		return true;
+		
+	}
+	
+	assignQuery() {
+		
+		//Check for queue entries and assign free machines in order of preference
+		//Greater zones first
+		while (this.queryQueue.length > 0 && this.freeMachines.length > 0) {
+			
+			let maxZone = 0;
+			let maxZoneIndex = 0;
+			
+			for (let i = 0; i < this.queryQueue.length; i++) {
+				
+				if (this.queryQueue[i][0] > maxZone) {
+					
+					maxZone = this.queryQueue[i][0];
+					maxZoneIndex = i;
+					
+				}
+				
+			}
+			
+			this.freeMachines[0].sendWork(this.queryQueue[maxZoneIndex]);
+			
+			//Add the machine to busyMachines
+			this.busyMachines.push(this.freeMachines[0]);
+			
+			//Remove the machine from freeMachines
+			this.freeMachines.splice(0, 1);
+			
+		}
+		
+	}
+	
+	//Move machines that finished its jobs to freeMachines
+	freeTheMachines() {
+		
+		for(let i = this.busyMachines.length; i > 0; i--) {
+			
+			if (this.busyMachines[i].machineStatus == "DONE") {
+				
+				this.freeMachines.push(this.busyMachines[i]);
+				this.busyMachines.splice(i, 1);
+				
+			}
+			
+		}
+		
+	}
+	
+	//Function to be called from all different machines
+	swap(a, b) {
+		
+		//Swap the value if both are in the same zone
+		if (this.dataStatus[a] != this.dataStatus[b]) {return false;}
+		
+		let temp = this.data[a];
+		this.data[a] = this.data[b];
+		this.data[b] = temp;
+		
+		return true;
 		
 	}
 	
 	step() {
 		
-		for (let i = 0; i < this.machines.length) {this.machines[i].step();}
+		if (this.controllerStatus == "OFF") {this.set(); return false;}
+		if (this.controllerStatus == "DONE") {return false;}
+		
+		for (let i = 0; i < this.machines.length; i++) {
+			
+			if (this.machines[i].step() == false) {return false;}
+			
+		}
+		
+		return true;
+		
+	}
+	
+	query(n, machine) {
+		
+		this.queryQueue.push([n, machine]);
 		
 	}
 	
--- machine.js
+++ machine.js
@@ -1,18 +1,51 @@
 class Machine {
 	
-	constructor() {
+	constructor(controller) {
 		
+		this.controller = controller;
 		this.data = [];
 		this.avg = 0;
 		this.machineStatus = "OFF";
+		this.leftPivot = -1;
+		this.rightPivot = -1;
+		
+	}
+	
+	setMachine(a, b) {
+		
+		this.leftPivot = a;
+		this.rightPivot = b;
+		
+		let avg = 0;
+		
+		for(let i = 0; i < this.data.length; i++) {
+			
+			avg = avg - ((avg + this.data[i])/i);
+			
+		}
+		
+		this.avg = avg;
 		
 	}
 	
 	step() {
 		
-		if (this.machineStatus != "WORK") {return false;}
+		if (this.machineStatus == "OFF") {return false;}
 		
+		if (this.machineStatus == "WORK") {
+			
+			if (leftPivot >= rightPivot) {this.machineStatus == "DONE"; return true;}
+			
+			if (this.data[leftPivot] > this.avg) {this.controller.swap(leftPivot, rightPivot); rightPivot--; return true;}
+			
+			if (this.data[leftPivot] <= this.avg) {leftPivot++; return true;}
+			
+		}
 		
+		if (this.machineStatus == "DONE") {return true;}
+		
+		//Step should never reach this line
+		return false;
 		
 	}
 	
